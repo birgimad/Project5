62b0ffb74a07232e207727bdf63afe6ee3c282a1
birgimad <birgitte92madsen@gmail.com>
2015-12-02 11:35:59 +0100

    Cleaning RK4 and adding comments

--- Cpp/RK4_for_a/main.cpp
+++ Cpp/RK4_for_a/main.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 using namespace arma;
 
-void Derivative(double (&r)[3], double (&v)[3], double (&drdt)[3], double (&dvdt)[3], double G, double mass){
-    //int n = 3;       // use evt: sizeof(drdt)/sizeof(drdt[0])
+void Derivative(double r[3], double v[3], double (&drdt)[3], double (&dvdt)[3], double G, double mass){
     drdt[0] = v[0];
     drdt[1] = v[1];
     drdt[2] = v[2];
@@ -15,49 +14,44 @@ void Derivative(double (&r)[3], double (&v)[3], double (&drdt)[3], double (&dvdt
     dvdt[1] = newtonian_force*r[1];
     dvdt[2] = newtonian_force*r[2];
 }
-/*
-void updating_dummies(double dt, double drdt, double dvdt, double (&r_dummy)[3], double (&v_dummy)[3], int number)
+
+void updating_dummies(double dt, double drdt[3], double dvdt[3], double (&r_dummy)[3], double (&v_dummy)[3], double number, double (&kr)[3], double (&kv)[3], double r[3], double v[3])
 {
-    double kr[n];
-    double kv[n];
-    for (int i = 0; i<n; i++){
+    for (int i = 0; i<3; i++){
         kr[i] = dt*drdt[i];
         kv[i] = dt*dvdt[i];
         r_dummy[i] = r[i] +kr[i]/number;
         v_dummy[i] = v[i] +kv[i]/number;
     }
-} */
+}
 
 
 int main()
 {
     int n=3;
 
-    double G = 2.96e-4;
-    double mass_earth = 3e-6;
-    double mass = 1;
+    double G = 2.96e-4;     //Grav const in the units of AU^3 / ( days^3 * mass_sun )
+    double mass_earth = 3e-6;       //mass in the units of mass_sun
+    double mass = 1;        //mass in the units of mass_sun
     double t0 = 0.0;
-    double t_final = 365.0;
+    double t_final = 365.0;     //time in the unit of days
     int number_of_time_step = 100000;
     double time = t0;
     double dt = (t_final - t0)/number_of_time_step;
 
-    double r[3];
+    double r[3], v[3], drdt[3], dvdt[3], r_dummy[3], v_dummy[3];
+    double k1r[3], k2r[3], k3r[3], k4r[3];
+    double k1v[3], k2v[3], k3v[3], k4v[3];
+
+    //initializing relative position
     r[0] = 1.0;
     r[1] = 0.0;
     r[2] = 0.0;
-    double v[3];
+    //initializing relative velocity
     v[0] = 0;
     v[1] = 0.017;
     v[2] = 0;
-    double drdt[3];
-    drdt[0] = 0;
-    drdt[1] = 0;
-    drdt[2] = 0;
-    double dvdt[3];
-    drdt[0] = 0;
-    drdt[1] = 0;
-    drdt[2] = 0;
+
     cout << "initial position:" << endl;
     for (int i = 0; i<n; i++)
     {
@@ -68,57 +62,34 @@ int main()
     {
         cout << v[i] << endl;
     }
-    double energy_initial = v[0]*v[0]+v[1]*v[1]+v[2]*v[2]*0.5+G*mass*pow(r[0]*r[0]+r[1]*r[1]+r[2]*r[2],-0.5);
+
+    double distance_sun_initial = pow(r[0]*r[0]+r[1]*r[1]+r[2]*r[2],1/2);
+    cout << "Distance to sun: " << distance_sun_initial << " AU" << endl;
+
     // energy calculated in the units of J/mass_earth (orbiting object)
+    double energy_initial = v[0]*v[0]+v[1]*v[1]+v[2]*v[2]*0.5+G*mass*pow(r[0]*r[0]+r[1]*r[1]+r[2]*r[2],-0.5);
     cout << "Energy_initial = " << energy_initial << endl;
 
-
+    //RK4:
     while(time<=t_final){
     Derivative(r,v,drdt,dvdt,G,mass);
-
-    double k1r[n];
-    double k1v[n];
-    double r_dummy[3];
-    double v_dummy[3];
-    for (int i = 0; i<n; i++){
-        k1r[i] = dt*drdt[i];
-        k1v[i] = dt*dvdt[i];
-        r_dummy[i] = r[i] +k1r[i]/2.0;
-        v_dummy[i] = v[i] +k1v[i]/2.0;
-    }
+    updating_dummies(dt,drdt,dvdt,r_dummy,v_dummy,2,k1r,k1v,r,v);
     Derivative(r_dummy,v_dummy,drdt,dvdt,G,mass);
-    double k2r[n];
-    double k2v[n];
-    for (int i = 0; i<n; i++){
-        k2r[i] = dt*drdt[i];
-        k2v[i] = dt*dvdt[i];
-        r_dummy[i] = r[i] +k2r[i]/2.0;
-        v_dummy[i] = v[i] +k2v[i]/2.0;
-    }
+    updating_dummies(dt,drdt,dvdt,r_dummy,v_dummy,2,k2r,k2v,r,v);
     Derivative(r_dummy,v_dummy,drdt,dvdt,G,mass);
-    double k3r[n];
-    double k3v[n];
-    for (int i = 0; i<n; i++){
-        k3r[i] = dt*drdt[i];
-        k3v[i] = dt*dvdt[i];
-        r_dummy[i] = r[i] +k3r[i];
-        v_dummy[i] = v[i] +k3v[i];
-    }
+    updating_dummies(dt,drdt,dvdt,r_dummy,v_dummy,1,k3r,k3v,r,v);
     Derivative(r_dummy,v_dummy,drdt,dvdt,G,mass);
-    double k4r[n];
-    double k4v[n];
     for (int i = 0; i<n; i++){
         k4r[i] = dt*drdt[i];
         k4v[i] = dt*dvdt[i];
-
     }
-
     for (int i=0; i<n;i++){
         r[i] = r[i] +(1.0/6.0)*(k1r[i]+2*k2r[i]+2*k3r[i]+k4r[i]);
         v[i] = v[i] +(1.0/6.0)*(k1v[i]+2*k2v[i]+2*k3v[i]+k4v[i]);
     }
     time += dt;
     }
+
     cout << "final position:" << endl;
     for (int i = 0; i<n; i++)
     {
@@ -130,8 +101,12 @@ int main()
         cout << v[i] << endl;
     }
 
+    double distance_sun_final = pow(r[0]*r[0]+r[1]*r[1]+r[2]*r[2],1/2);
+    cout << "Distance to sun: " << distance_sun_final << " AU" << endl;
+
     double energy_final = v[0]*v[0]+v[1]*v[1]+v[2]*v[2]*0.5+G*mass*pow(r[0]*r[0]+r[1]*r[1]+r[2]*r[2],-0.5);
-    cout << "Energy_final = " << energy_final<< endl;
+    cout << "Energy_final = " << energy_final << endl;
+
     return 0;
 }
 
