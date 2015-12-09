2fdbe956955bf859eea3d6ab4e595df4aec8ce33
birgimad <birgitte92madsen@gmail.com>
2015-12-09 10:20:46 +0100

    fixing some fxnotes in report

--- Graphs/Probability.m
+++ Graphs/Probability.m
@@ -11,6 +11,9 @@ xlRange = 'A1:A100000';
 [v,T,vT] = xlsread(filename, sheet, xlRange);
 data_random_mass=v(:,1); 
 
+y = [7:.1:13];
+norm = normpdf(y,10,1);
+
 %Plot histograms
 figure
 
@@ -19,6 +22,12 @@ xbins = 7:0.5:13;
 [f,x] = hist(data_random_mass/10000,xbins)
 dx = diff(x(1:2));
 bar(x,f/sum(f*dx))
+
+hold on
+
+plot(y,norm,'r','LineWidth',2)
+
+legend('Generated masses','Gaussian dist.')
 
 xlabel('Mass in unit of solar masses', 'fontsize',14) % x-axis label
 ylabel('Probability','fontsize',14) % y-axis label
